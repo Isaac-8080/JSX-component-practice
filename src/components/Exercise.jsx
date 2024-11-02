ad87e3ec33f10bbfc6295d5f91c7d65b52e2e15c
Isaac Opoku <isaacmanuopoku@gmail.com>
2024-11-02 05:33:47 +0000

    Exercise component added

---
+++ src/components/Exercise.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Exercise = () => {
+  return (
+    <>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, voluptatibus incidunt blanditiis perspiciatis voluptates quisquam labore. Blanditiis nemo repellendus voluptatum dolor, reiciendis architecto sunt nisi doloremque molestiae incidunt perferendis ratione quasi dignissimos minima suscipit fuga voluptatibus voluptate eius neque rem voluptas! Sequi quaerat quisquam esse adipisci commodi velit fuga, harum laudantium repellendus at. Voluptatibus natus in repudiandae. Modi molestias illo soluta facere ipsam fugit autem quae quo? Facere rem et voluptatibus! Quasi beatae ducimus eaque harum voluptas libero officiis ratione quibusdam in corrupti, officia quo soluta, tempora amet odio voluptatibus unde accusamus necessitatibus quaerat vel cupiditate saepe dicta. Dolorem dolore optio dolores rerum magnam qui porro quibusdam doloribus. Reiciendis veniam porro exercitationem ea at rem quia perspiciatis dicta aliquam. Distinctio vero veniam obcaecati?
+
+      </p>
+      {/* <img src={reactLogo} alt="" /> */}
+    </>
+  )
+}
+
+export default Exercise;
